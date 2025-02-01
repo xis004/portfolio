fb45513ec978366422d08012eef0d16fdf398d98
xiangyshi <xiangy.shi@gmail.com>
2025-01-31 20:02:37 -0800

    path update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
-const projects = await fetchJSON('/lib/projects.json');
+const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 
 const projectsContainer = document.querySelector('.projects');
